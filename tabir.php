72c8e5464f17189ac02fbb97f99334fdb9b1e9d3
abootalex <39976551+abootalex@users.noreply.github.com>
2018-06-06 07:08:39 +0430

    Create tabir.php

---
+++ tabir.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* Plugin Name: wp tabire khab
+* Description: تعبیر خواب
+* Version: 1.0
+* Author: ابوطالب
+* Author URI: http://hamdore.com/
+*/
+
+// Example  : WP Shortcode to display form on any page or post.
+function form_creation(){
+?>
